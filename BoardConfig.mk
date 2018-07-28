ac14cc6eb84899bdb36f3e9bb7ebc917cb540818
AndroPlus <mail@andro.plus>
2018-07-28 13:35:58 +0900

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,63 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6763
+TARGET_BOOTLOADER_BOARD_NAME := g36v63c2k_kw
+TARGET_NO_BOOTLOADER := true
+
+# Extensions
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+TARGET_CPU_CORTEX_A53 := true
+
+# File System
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Kernel
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x14f88000 --second_offset 0x00e88000 --tags_offset 0x13f88000 --board 1480056755
+TARGET_PREBUILT_KERNEL := device/umidigi/z2/prebuilt/zImage
+
+# Resolution
+DEVICE_RESOLUTION := 1080x2246
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2246
+
+# TWRP
+# TW_DEVICE_VERSION := $(shell date -u +"%Y%m%d_%H%M")
+# TW_ALWAYS_RMRF := true
+TW_INCLUDE_FB2PNG := true
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
